06265da84374f9eda0cc196e7c87e4d83faee199
Tom White <twhite@rightpoint.com>
2026-01-14 12:12:37 -0500

    try footer update

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -16,5 +16,35 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  // Structure the footer content
+  const wrapper = footer.querySelector('.default-content-wrapper');
+  if (wrapper) {
+    // Find and style logo
+    const logoImg = wrapper.querySelector('picture img, img');
+    if (logoImg) {
+      logoImg.closest('p')?.classList.add('footer-logo-row');
+    }
+
+    // Find and style social links (links with icons)
+    const socialLinks = wrapper.querySelectorAll('a .icon, a > span.icon');
+    if (socialLinks.length > 0) {
+      socialLinks.forEach((icon) => {
+        const link = icon.closest('a');
+        if (link) {
+          link.classList.add('footer-social-link');
+        }
+      });
+    }
+
+    // Find copyright text (usually the last paragraph)
+    const paragraphs = wrapper.querySelectorAll('p');
+    if (paragraphs.length > 0) {
+      const lastP = paragraphs[paragraphs.length - 1];
+      if (lastP.textContent.toLowerCase().includes('copyright') || lastP.textContent.includes('©')) {
+        lastP.classList.add('footer-copyright');
+      }
+    }
+  }
+
   block.append(footer);
 }
