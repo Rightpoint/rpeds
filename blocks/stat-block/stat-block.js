20cfc16958a41122c8819d12476e2c9c250714f9
Tom White <twhite@rightpoint.com>
2026-01-19 10:27:24 -0500

    testing

--- blocks/stat-block/stat-block.js
+++ blocks/stat-block/stat-block.js
@@ -1,4 +1,19 @@
 
 export default function decorate(block) {
- console.log("Rendering Stat Block");
+ console.log("Begin rendering Stat Block");
+   const items = [...block.children];
+  const container = document.createElement('div');
+  container.className = 'stat-block-container';
+
+  const animatedElements = [];
+
+  items.forEach((item) => {
+    const stat = document.createElement('div');
+    stat.className = 'stat-block-item';
+
+    const valueDiv = item.children[0];
+    const labelDiv = item.children[1];
+    
+  })
+  console.log("After render stat block")
 }
